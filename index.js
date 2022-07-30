41a8a0952aa66578990f9977221bc119d3172f55
Justina Mandaro <bautistajustina7@gmail.com>
2022-07-29 18:41:40 -0700

    commit tree cd97b99f6d2208329b4adf34f4550a3996bf5df1

--- index.js
+++ index.js
@@ -1,1 +1,22 @@
 // code your solution here
+function saturdayFun(activity="roller-skate") {
+    return(`This Saturday, I want to ${activity}!`);
+}
+saturdayFun();
+
+const mondayWork = function(monActivity="go to the office"){
+    return(`This Monday, I will ${monActivity}.`)
+}
+mondayWork("play tennis");
+
+function wrapAdjective(parameter1="&"){
+    const part1 = "You are";
+    return function(part2){
+        return(`${part1} ${parameter1}${part2}${parameter1}!`)
+
+    };
+}
+
+wrapAdjective("%")("a dedicated programmer");
+wrapAdjective("*")("a hard worker");
+wrapAdjective("||")("a dedicated programmer")
